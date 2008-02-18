be6b5b4576b776cab94d6c307392a23334a50ecf
tomgross <itconsense@gmail.com>
2008-02-18 15:42:51 +0000

    moved encodings and utility methods to bibliograph.core

--- bibliograph/core/tests.py
+++ bibliograph/core/tests.py
@@ -1,17 +1,30 @@
-import unittest
+import unittest, doctest
 
 from zope.testing import doctestunit
 from zope.component import testing
-from Testing import ZopeTestCase as ztc
+
+class DummyEntry(object):
+
+    def __init__(self, id=None):
+        self.id = id
+
+    def getId(self):
+        return self.id
 
 def test_suite():
     return unittest.TestSuite([
 
         # Unit tests for your API
         doctestunit.DocFileSuite(
-            'README.txt', package='bibliograph.core',
-            setUp=testing.setUp, tearDown=testing.tearDown),
-
+            'README.txt',
+            package='bibliograph.core',
+            setUp=testing.setUp,
+            tearDown=testing.tearDown),
+            
+        doctestunit.DocTestSuite(
+            'bibliograph.core.utils',
+            optionflags=doctest.ELLIPSIS,
+            globs=dict(DummyEntry=DummyEntry))
         ])
 
 if __name__ == '__main__':
